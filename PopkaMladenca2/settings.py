102a1edbb5130f3ada1b02fe2075f1f557aa3d37
cvrsxxxd <Kostya@gmail.com>
2023-01-27 18:06:17 +0500

    Registration2

--- PopkaMladenca2/settings.py
+++ PopkaMladenca2/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhitenNoiseMiddleWare', # Только для сервера<<
+    'whitenoise.middleware.WhiteNoiseMiddleware', # Только для сервера<<
 ]
 
 ROOT_URLCONF = 'PopkaMladenca2.urls'
